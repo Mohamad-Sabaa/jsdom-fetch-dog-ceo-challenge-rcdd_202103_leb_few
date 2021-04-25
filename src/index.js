b5dc99ba6d6af9d1e6b0b8ac310ec06986ad7a4c
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-04-25 18:06:59 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 console.log('%c HI', 'color: firebrick')
 document.addEventListener('DOMContentLoaded', fetchImages)
-
+const images = document.getElementById("dog-image-container")
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 
@@ -11,7 +11,15 @@ function fetchImages () {
   })
   .then(function(json) {
     console.log(json);
+    for(let i = 0; i < images.length; i++)
+    images.insertAdjacentHTML('beforeend', `<img src=$(json(message[1]))`);
   });
+
+  
 }
+
+
+
+
 
 //fetchImages(image);
