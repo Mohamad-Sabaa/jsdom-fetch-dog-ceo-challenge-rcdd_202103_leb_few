c280a0cee06e645a9463e55e7d010b2958a88744
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-04-25 17:38:15 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log('%c HI', 'color: firebrick')
+const image = document.getElementById("dog-image-container");
+function fetchImages (image) {
+  return fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+  });
+}
+
+//fetchImages(image);
